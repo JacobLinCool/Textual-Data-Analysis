9f0362ee542b5db869c0c200085d454db989d7ed
JacobLinCool <jacoblincool@gmail.com>
2022-01-18 21:43:44 +0800

    feat: if values are Set sort_map will sort by Set size

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,17 @@ export function escape_regex(str: string): string {
     return str.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
 }
 
-export function sort_map<T>(map: Map<T, number>): Map<T, number> {
-    const sorted = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
+export function sort_map<T, K>(map: Map<T, K>): Map<T, K> {
+    const sorted = new Map(
+        [...map.entries()].sort((a, b) => {
+            if (b[1] instanceof Set && a[1] instanceof Set) {
+                return b[1].size - a[1].size;
+            } else if (typeof a[1] === "number" && typeof b[1] === "number") {
+                return b[1] - a[1];
+            } else {
+                return b[1] < a[1] ? 1 : -1;
+            }
+        }),
+    );
     return sorted;
 }
